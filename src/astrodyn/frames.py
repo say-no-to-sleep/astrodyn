5fbafdc4dce261b5ccaf1f3c057fe123d636e246
say-no-to-sleep <genezhao63@proton.me>
2026-08-30 21:39:55 -0400

    Added frames and tests

--- src/astrodyn/frames.py
+++ src/astrodyn/frames.py
@@ -2,7 +2,7 @@ import numpy as np
 from astrodyn.states import StateVector
 from astrodyn.states import ClassicalElements
 
-def perifocal_to_eci(r_pf: np.ndarray, v_pf: np.ndarray, Omega: float, i: float, omega: float) -> StateVector:
+def _perifocal_eci_conversion(r: np.ndarray, v: np.ndarray, Omega: float, i: float, omega: float, direction: str) -> StateVector:
     # Build Q
 
     ## Build R3(omega)
@@ -23,8 +23,21 @@ def perifocal_to_eci(r_pf: np.ndarray, v_pf: np.ndarray, Omega: float, i: float,
 
     Q = np.matmul(np.matmul(R3_omega, R1_i), R3_Omega)
 
-    r_x = np.matmul(Q.T, r_pf)
-    v_x = np.matmul(Q.T, v_pf)
+    if direction == "pf_to_eci":
+        Q = Q.T
+    elif direction == "eci_to_pf":
+        Q = Q
+    else:
+        raise ValueError('Invalid direction "%s", choose between "pf_to_eci" and "eci_to_pf"', direction)
+
+    r_x = np.matmul(Q, r)
+    v_x = np.matmul(Q, v)
 
     state = StateVector(r_x, v_x)
     return state
+
+def perifocal_to_eci(r_pf: np.ndarray, v_pf: np.ndarray, Omega: float, i: float, omega: float) -> StateVector:
+    return _perifocal_eci_conversion(r_pf, v_pf, Omega, i, omega, "pf_to_eci")
+
+def eci_to_perifocal(r_eci: np.ndarray, v_eci: np.ndarray, Omega: float, i: float, omega: float) -> StateVector:
+    return _perifocal_eci_conversion(r_eci, v_eci, Omega, i, omega, "eci_to_pf")
---
+++ tests/test_frames.py
@@ -0,0 +1,46 @@
+import numpy as np
+import astrodyn.frames
+import astrodyn.states
+import astrodyn.elements
+from astrodyn.constants import MU_EARTH
+
+def test_zero_angle_perifocal_to_eci_and_back():
+    """Test known value with 0 angles to eci and back
+    """
+    r = np.array([-3670, -3870, 4400])
+    v = np.array([4.7, -7.4, 1])
+
+    eci = astrodyn.frames.perifocal_to_eci(r, v, 0, 0, 0)
+
+    recovered = astrodyn.frames.eci_to_perifocal(eci.r_vec, eci.v_vec, 0, 0, 0)
+
+    np.testing.assert_allclose(recovered.r_vec, r, atol=1e-10)
+    np.testing.assert_allclose(recovered.v_vec, v, atol=1e-10)
+
+
+
+def test_perifocal_to_eci_and_back():
+    """Test known value with plausible angles to eci and back
+    """
+    r = np.array([-3670, -3870, 4400])
+    v = np.array([4.7, -7.4, 1])
+
+    eci = astrodyn.frames.perifocal_to_eci(r, v, np.deg2rad(40), np.deg2rad(30), np.deg2rad(60))
+
+    recovered = astrodyn.frames.eci_to_perifocal(eci.r_vec, eci.v_vec, np.deg2rad(40), np.deg2rad(30), np.deg2rad(60))
+
+    np.testing.assert_allclose(recovered.r_vec, r, atol=1e-10)
+    np.testing.assert_allclose(recovered.v_vec, v, atol=1e-10)
+
+
+
+def test_eci_to_perifocal_known_value():
+    """Test known r, v, and hand calculate Q to compare calculated value to expected value
+    """
+    r = np.array([1, 0, 0])
+    v = np.array([0, 1, 0])
+
+    eci = astrodyn.frames.eci_to_perifocal(r, v, 0, np.deg2rad(90), 0)
+
+    np.testing.assert_allclose(eci.r_vec, np.array([1, 0, 0]), atol = 1e-10)
+    np.testing.assert_allclose(eci.v_vec, np.array([0, 0, -1]), atol = 1e-10)
