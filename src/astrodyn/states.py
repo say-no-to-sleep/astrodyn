c40719a7387facde203ec501624d23af9a4f40cc
say-no-to-sleep <genezhao63@proton.me>
2026-09-05 17:02:09 -0400

    Improved handling of parabolic orbits. Bumped version to 0.1.1

--- src/astrodyn/states.py
+++ src/astrodyn/states.py
@@ -2,6 +2,9 @@ import numpy as np
 from dataclasses import dataclass
 from typing import NamedTuple
 
+# Tolerance for orbits near Parabola
+PARABOLIC_TOLERANCE = 1e-8
+
 class StateVector(NamedTuple):
     r_vec: np.ndarray
     v_vec: np.ndarray
@@ -17,11 +20,13 @@ class ClassicalElements:
 
     # Calculate the semimajor axis, a
     def semimajor_axis(self, mu: float) -> float:
+        if abs(1.0 - self.e) < PARABOLIC_TOLERANCE:
+            return float("inf")
         return self.h**2 / (mu * (1-self.e**2))
 
     # Calculate the Period (only for elliptical), T
     def period(self, mu: float) -> float | None:
-        if self.e >= 1:
+        if self.e >= (1.0 - PARABOLIC_TOLERANCE):
             return None
         return (2 * np.pi * self.semimajor_axis(mu)**(3/2)) / np.sqrt(mu)
 
@@ -31,12 +36,13 @@ class ClassicalElements:
 
     # Calculate the apoapsis radius, r_a
     def apoapsis_radius(self, mu: float) -> float | None:
-        if self.e >= 1:
+        if self.e >= (1.0 - PARABOLIC_TOLERANCE):
             return None
         return self.h**2 / (mu * (1 - self.e))
 
     # Calculate the specific energy, epsilon
     def specific_energy(self, mu: float) -> float:
-        return -mu / (2 * self.semimajor_axis(mu))
-
+        # Update: use identity relating specific orbital energy 
+        # directly to angular momentum and eccentricity
+        return -(mu**2 / (2 * self.h**2)) * (1 - self.e**2)
     
